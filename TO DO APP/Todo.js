688019f58fe95b4522cbc3b099eb0c21d3cdd95e
muskanmohiuddin <cpie6302@gmail.com>
2024-08-07 02:01:05 -0700

    last commit

--- TO DO APP/Todo.js
+++ TO DO APP/Todo.js
@@ -1,4 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const storedTasks = JSON.parse(localStorage.getItem("tasks"));
+
+  if (storedTasks) {
+    storedTasks.forEach((task) => tasks.push(task));
+    updateTasksList();
+    updateStats();
+  }
+});
+
 let tasks = [];
+
+const saveTasks = () => {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+};
 
 const addTask = () => {
   const taskInput = document.getElementById("taskInput");
@@ -7,8 +21,45 @@ const addTask = () => {
     tasks.push({ text: text, completed: false });
     taskInput.value = "";
     updateTasksList();
+    updateStats();
+    saveTasks();
   }
 };
+
+const toggleTaskComplete = (index) => {
+  tasks[index].completed = !tasks[index].completed;
+  updateTasksList();
+  updateStats();
+  saveTasks();
+};
+const deleteTask = (index) => {
+  tasks.splice(index, 1);
+  updateTasksList();
+  updateStats();
+  saveTasks();
+};
+const editTask = (index) => {
+  const taskInput = document.getElementById("taskInput");
+  taskInput.value = tasks[index].text;
+
+  tasks.splice(index, 1);
+  updateTasksList();
+  updateStats();
+  saveTasks();
+};
+
+const updateStats = () => {
+  const completeTasks = tasks.filter((task) => task.completed).length;
+  const totalTasks = tasks.length;
+  const progress = (completeTasks / totalTasks) * 100;
+  const progressBar = document.getElementById("progress");
+  console.log(progress);
+  progressBar.style.width = $`{progress}%`;
+  document.getElementById(
+    "numbers"
+  ).innerText = `${completedTasks} / ${totalTasks}`;
+};
+
 const updateTasksList = () => {
   const taskList = document.getElementById("task-list");
   taskList.innerHTML = "";
